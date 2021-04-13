b4d83e2ab5ac873fd50123d153877fc86c9caba9
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-04-13 14:54:51 -0700

    * adds a Button and a mutableStateOf var, updating UI with remember, calculated during recomposition * interesting bug, when accessing stringResource with named param id=... the value of count does not get passed * transparent Divider used for spacing

--- app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/composebasicscodelab/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
@@ -44,7 +44,10 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")){
         Divider(color = Color.Black)
 
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter()
     }
+
 }
 
 @Composable
@@ -55,6 +58,15 @@ fun Greeting(name: String) {
 //        .size(28.dp)
         .padding(12.dp)
         )
+}
+
+@Composable
+fun Counter(){
+    val count = remember { mutableStateOf(0)}
+    
+    Button(onClick = {count.value++}){
+        Text(stringResource(R.string.counter_text, count.value))
+    }
 }
 
 @Preview(showBackground = true, name = "default_preview")
